42aa7c3e3c46501d6c18c490f44af7803cf7f491
skyfour2018 <skyjiapeng@sina.com>
2018-12-07 18:32:06 +0800

    modify log out put console

--- pkg/utils/log.go
+++ pkg/utils/log.go
@@ -24,10 +24,24 @@ func init() {
 	Log.Hooks = make(logrus.LevelHooks)
 	Log.Level = logrus.InfoLevel
 
+	path := "../var/logs/"
+	_, err := os.Stat(path)
+	if err != nil {
+		// create dir
+		err = os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir failed![%v][path:%s]\n", err, path)
+		}
+	}
+	logOut := Config.GetString("log.out")
 	filename := time.Now().Format("2006-01-02")
-	Log.OSFile, Log.err = os.OpenFile(fmt.Sprintf("../var/logs/%s.log", filename), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	Log.OSFile, Log.err = os.OpenFile(fmt.Sprintf(path + "%s.log", filename), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if Log.err == nil {
-		Log.Out = Log.OSFile
+		if logOut == "console" {
+			Log.Out = os.Stdout
+		} else {
+			Log.Out = Log.OSFile
+		}
 	} else {
 		fmt.Printf("Failed to log to file - %s.log, using default stderr\n", filename)
 	}
