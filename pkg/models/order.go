84d55ba49b704da540fc64fe93eee273af1b44d9
skyfour2018 <skyjiapeng@sina.com>
2018-12-08 16:55:41 +0800

    update order models

--- pkg/models/order.go
+++ pkg/models/order.go
@@ -1,12 +1,37 @@
 package models
 
 type Order struct {
-	Id       int64       `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
-	BuyerId  int64       `gorm:"type:" json:"buyer_id"`
-	SellerId int64       `gorm:"" json:"buyer_id"`
-	Price    float32     `json:"price"`
-	Quantity float64     `json:"quantity"`
-	Status   OrderStatus `gorm:"type:int"`
+	OrderNumber int64   `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
+	Price       float32 `gorm:"type:decimal(10,2)" json:"price"`
+	//成交量
+	Quantity string  `gorm:"type:varchar(32)"json:"quantity"`
+	Amount   float64 `gorm:"type:decimal(20,5)" json:"amount"`
+	//成交额
+	PaymentRef string `gorm:"type:varchar(8)" json:"payment_ref"`
+	//订单状态
+	Status OrderStatus `gorm:"type:tinyint(1)" json:"status"`
+	//成交方向，以发起方也就是
+	Direction         int `gorm:"type:tinyint(1)" json:"direction"`
+	DistributorId     int `gorm:"type:int(11)" json:"distributor_id"`
+	MerchantId        int `gorm:"type:int(11)" json:"merchant_id"`
+	MerchantPaymentId int `gorm:"type:int(11)" json:"merchant_payment_id"`
+	//交易币种
+	CurrencyCrypto string `gorm:"type:varchar(30)" json:"currency_crypto"example:"BTUSD"`
+	//交易法币
+	CurrencyFiat string `gorm:"type:char(3)" json:"currency_fiat" example:"RMB"`
+	//交易类型 0:微信,1:支付宝,2:银行卡
+	PayType uint `gorm:"column:pay_type;type:tinyint(2)" json:"pay_type"`
+	//微信或支付宝二维码地址
+	QrCode string `gorm:"type:varchar(255)"`
+	//微信或支付宝账号
+	Name string `gorm:"type:varchar(100)" json:"name"`
+	//银行账号
+	BankAccount string `gorm:"" json:"bank_account"`
+	//所属银行
+	Bank string `gorm:"" json:"bank"`
+	//所属银行分行
+	BankBranch string `gorm:"" json:"bank_branch"`
+	Timestamp
 }
 
 type OrderStatus int
