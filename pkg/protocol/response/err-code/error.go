61486282ca250db67032c36c17c6db5ab7a88107
xyyhcl <1245815983@qq.com>
2018-12-13 15:41:31 +0800

    Optimize the error_code format

--- pkg/protocol/response/err-code/error.go
+++ pkg/protocol/response/err-code/error.go
@@ -10,14 +10,13 @@ func (e Err) Data() (int, string) {
 }
 
 var (
-
-	DistributorErr    = Err{20100, "create distributor is failed."}
-	RequestParamErr   = Err{20001, "request param is error."}
-	CreateUserErr     = Err{20400, "create user is failed."}
-	NotFoundMerchant  = Err{20201, "not found merchant"}
-	NotFoundUser      = Err{20401, "not found user"}
-	UserPasswordError = Err{20402, "user password is invalid"}
-	NoAccountIdOrDistributorIdErr=Err{20403, "accountId or distributorId is null"}
-	NoOrderNumberErr=Err{20404,"orderNumber is null"}
-	NoOrderFindErr=Err{20405,"no order found by orderNumber"}
+	DistributorErr                = Err{20100, "create distributor is failed."}
+	RequestParamErr               = Err{20001, "request param is error."}
+	CreateUserErr                 = Err{20400, "create user is failed."}
+	NotFoundMerchant              = Err{20201, "not found merchant"}
+	NotFoundUser                  = Err{20401, "not found user"}
+	UserPasswordError             = Err{20402, "user password is invalid"}
+	NoAccountIdOrDistributorIdErr = Err{20403, "accountId or distributorId is null"}
+	NoOrderNumberErr              = Err{20404, "orderNumber is null"}
+	NoOrderFindErr                = Err{20405, "no order found by orderNumber"}
 )
