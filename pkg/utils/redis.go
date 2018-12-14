88a70215873629eeadaf0aa256401c2d9d295317
skyfour2018 <skyjiapeng@sina.com>
2018-12-14 12:41:37 +0800

    add merchant online redis key and get function

--- pkg/utils/redis.go
+++ pkg/utils/redis.go
@@ -7,11 +7,21 @@ import (
 	"time"
 )
 
-var RedisClient = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
+var (
+	// Redis - Global Redis Client Ref.
+	RedisClient *redis.Client
+	// KeyPrefix key prefix
+	KeyPrefix string
+)
+
+func init() {
+	// init redis client with options
+	options := redis.Options{
+		Addr:     Config.GetString("cache.redis.host") + ":" + Config.GetString("cache.redis.port"),
+		Password: Config.GetString("cache.redis.password")}
+	RedisClient = redis.NewClient(&options)
+	KeyPrefix = Config.GetString("cache.redis.prefix")
+}
 
 func RedisSet(key string, value string, expiration time.Duration) error {
 	err := RedisClient.Set(key, value, expiration).Err()
@@ -44,4 +54,33 @@ func RedisVerifyValue(key string, val string) error {
 			return nil
 		}
 	}
+}
+
+func GetCacheSetMembers(key string) ([]string,error) {
+	all, err := RedisClient.SMembers(key).Result()
+	if err != nil {
+		Log.Warnf("Get set objects failed: %v", err)
+		return all,err
+	}
+	return all,nil
+}
+
+func SetCacheSetMember(key string,member interface{}) {
+	if err := RedisClient.SAdd(key,member).Err();err != nil {
+		Log.Warnf("Error in caching set of objects: %v", err)
+	}
+}
+
+func DelCacheSetMember(key string,member interface{}) {
+	if err := RedisClient.SRem(key,member).Err();err != nil {
+		Log.Warnf("Error in caching set of objects: %v", err)
+	}
+}
+
+func UniqueMerchantOnlineAutoKey() string {
+	return KeyPrefix + ":merchant:online:auto:list"
+}
+
+func UniqueMerchantOnlineKey() string {
+	return KeyPrefix + ":merchant:online:auto"
 }
