8f889d2dca838f9d166f2df2f118ff1799469c56
skyfour2018 <skyjiapeng@sina.com>
2018-12-19 16:01:51 +0800

    add get order detail mock data

--- pkg/controller/merchant_order.go
+++ pkg/controller/merchant_order.go
@@ -263,32 +263,60 @@ func GetOrdersByMerchant(c *gin.Context) {
 // @Success 200 {object} response.GetOrderDetailRet ""
 // @Router /m/merchants/{uid}/orders/{order-id} [get]
 func GetOrderDetail(c *gin.Context) {
-
+	orderNumber := c.Query("order-id")
 	var ret response.GetOrderDetailRet
 	ret.Status = response.StatusSucc
-	ret.Data = append(ret.Data, models.Order{
-		OrderNumber:       "10001",
-		Price:             4,
-		Quantity:          "",
-		Amount:            100,
-		PaymentRef:        "",
-		Status:            0,
-		Direction:         0,
-		DistributorId:     0,
-		MerchantId:        0,
-		MerchantPaymentId: 0,
-		AccountId:         "牛大春",
-		CurrencyCrypto:    "BTUSD",
-		CurrencyFiat:      "",
-		PayType:           0,
-		QrCode:            "",
-		Name:              "",
-		BankAccount:       "",
-		Bank:              "",
-		BankBranch:        "",
-		Timestamp:         models.Timestamp{
-			CreatedAt: time.Date(2018, 11, 17, 20, 34, 58, 651387237, time.UTC),
-		},
-	})
+	if orderNumber == "10001" {
+		ret.Data = append(ret.Data, models.Order{
+			OrderNumber:       "10001",
+			Price:             4.4,
+			Quantity:          "25",
+			Amount:            110,
+			PaymentRef:        "",
+			Status:            0,
+			Direction:         1,
+			DistributorId:     0,
+			MerchantId:        0,
+			MerchantPaymentId: 0,
+			AccountId:         "牛大春",
+			CurrencyCrypto:    "BTUSD",
+			CurrencyFiat:      "RMB",
+			PayType:           0,
+			QrCode:            "https://image.baidu.com/search/down?tn=download&word=download&ie=utf8&fr=detail&url=https%3A%2F%2Ftimgsa.baidu.com%2Ftimg%3Fimage%26quality%3D80%26size%3Db9999_10000%26sec%3D1545215853444%26di%3Dd9eae39078073fa50b6c75cfb4f6b4ce%26imgtype%3D0%26src%3Dhttp%253A%252F%252Fpic.chinaz.com%252F2018%252F0409%252F18040918011940118.jpg&thumburl=https%3A%2F%2Fss1.bdstatic.com%2F70cFvXSh_Q1YnxGkpoWK1HF6hhy%2Fit%2Fu%3D3021223247%2C92123690%26fm%3D26%26gp%3D0.jpg",
+			Name:              "",
+			BankAccount:       "",
+			Bank:              "",
+			BankBranch:        "",
+			Timestamp:         models.Timestamp{
+				CreatedAt: time.Date(2018, 11, 17, 20, 34, 58, 651387237, time.UTC),
+			},
+		})
+	} else if orderNumber == "10002" {
+		ret.Data = append(ret.Data, models.Order{
+			OrderNumber:       "10002",
+			Price:             6.5,
+			Quantity:          "20",
+			Amount:            130,
+			PaymentRef:        "",
+			Status:            0,
+			Direction:         0,
+			DistributorId:     0,
+			MerchantId:        0,
+			MerchantPaymentId: 0,
+			AccountId:         "牛二春",
+			CurrencyCrypto:    "BTUSD",
+			CurrencyFiat:      "RMB",
+			PayType:           1,
+			QrCode:            "http://13.250.12.109:8086/1_0_1_favicon.png",
+			Name:              "",
+			BankAccount:       "",
+			Bank:              "",
+			BankBranch:        "",
+			Timestamp:         models.Timestamp{
+				CreatedAt: time.Date(2018, 11, 17, 20, 34, 58, 651387237, time.UTC),
+			},
+		})
+	}
+
 	c.JSON(200, ret)
 }
