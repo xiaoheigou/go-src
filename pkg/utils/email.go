55b19f7c697c091d05931e93cc1a03c3c31c29eb
lucky1024 <xxx@xxx.com>
2018-12-19 14:36:59 +0800

    Use gopkg.in/gomail.v2 to send email

--- pkg/utils/email.go
+++ pkg/utils/email.go
@@ -1,67 +1,71 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
-	"net/smtp"
+	"gopkg.in/gomail.v2"
+	"net"
+	"strconv"
 	"strings"
 )
 
-func sendToMail(user, password, host, to, subject, body string) error {
-	hostPort := strings.Split(host, ":")
-	auth := smtp.PlainAuth("", user, password, hostPort[0])
+func sendMailUsingGomail(user, password, host, to, subject, body string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", user)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
 
-	// Setup headers
-	headers := make(map[string]string)
-	headers["From"] = user
-	headers["To"] = to
-	headers["Subject"] = subject
-	headers["Content-Type"] = "text/html; charset=UTF-8"
-
-	// Setup message
-	message := ""
-	for k,v := range headers {
-		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	onlyHost, onlyPort, _ := net.SplitHostPort(host)
+	var port int
+	var err error
+	if port, err = strconv.Atoi(onlyPort); err != nil {
+		Log.Errorln("strconv.Atoi error [%s]", err)
+		return err
 	}
-	message += "\r\n" + body
 
-	msg := []byte(message)
-	sendTo := strings.Split(to, ";")
-	err := smtp.SendMail(host, auth, user, sendTo, msg)
-	return err
+	d := gomail.NewDialer(onlyHost, port, user, password)
+
+	// Send the email
+	if err := d.DialAndSend(m); err != nil {
+		Log.Errorln("DialAndSend error [%s]", err)
+		return err
+	}
+	return nil
 }
 
 func SendRandomCodeToMail(to, randomCode, timeout string) error {
 	var user = Config.GetString("email.sender")
 	if ! IsValidEmail(user) {
 		Log.Errorln("Wrong configuration: email.sender [%v], not a valid email addr.", user)
-		return nil
+		return errors.New("wrong configuration: email.sender")
 	}
 	var password = Config.GetString("email.password")
 	if len(password) == 0 {
 		Log.Errorln("Wrong configuration: email.password [%v], it empty.", password)
-		return nil
+		return errors.New("wrong configuration: email.password")
 	}
 	var smtpSvr = Config.GetString("email.smtpsvr")
 	if ! strings.Contains(smtpSvr, ":") {
 		Log.Errorln("Wrong configuration: email.smtpsvr [%v], must contains host and port.", smtpSvr)
-		return nil
+		return errors.New("wrong configuration: email.smtpsvr")
 	}
 
-	subject := "您的Yuudidi注册随机码"
+	subject := "您的Yuudidi注册验证码"
 
 	body := `
 		<html>
 		<body>
 		<h3>
-		您的Yuudidi注册随机码为：  ` + randomCode + ` ，有效期 ` + timeout + ` 分钟，请您尽快验证。
+		您的Yuudidi注册验证码为：  ` + randomCode + ` ，有效期 ` + timeout + ` 分钟，请您尽快验证。
 		</h3>
 		</body>
 		</html>
 		`
 	fmt.Println("send email")
-	if err = sendToMail(user, password, smtpSvr, to, subject, body); err != nil {
+	if err := sendMailUsingGomail(user, password, smtpSvr, to, subject, body); err != nil {
 		Log.Errorln("SendRandomCodeToMail fail [%v]", err)
+		return err
 	}
 	return nil
 }
-
