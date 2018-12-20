89225603703b2a4072f411445f326bb983c7f5ad
skyfour2018 <skyjiapeng@sina.com>
2018-12-20 14:36:01 +0800

    modify err code

--- pkg/protocol/response/err-code/error.go
+++ pkg/protocol/response/err-code/error.go
@@ -13,16 +13,16 @@ var (
 	LoginErr                  = Err{20001, "invalid username or password"}
 	RequestParamErr           = Err{20002, "request param is error."}
 	CreateDistributorErr      = Err{20100, "create distributor is failed."}
-	CreateUserErr             = Err{20400, "create user is failed."}
-	UpdateUserErr             = Err{20202, "update user is failed"}
+	CreateUserErr             = Err{20200, "create user is failed."}
+	UpdateUserErr             = Err{20201, "update user is failed"}
 	ResetUserPasswordErr      = Err{20202, "reset user password is failed"}
-	OriginUserPasswordErr     = Err{20202, "user origin password is invalid"}
-	UpdateUserPasswordErr     = Err{20202, "update user password is failed"}
-	NotFoundUser              = Err{20401, "not found user"}
-	UpdateMerchantStatusErr   = Err{20202, "update merchant status is failed"}
-	NotFoundMerchant          = Err{20201, "not found merchant"}
-	CreateMerchantRechargeErr = Err{20203, "create recharge apply is failed"}
-	NotFoundAssetApplyErr     = Err{20204, "not found recharge apply"}
+	OriginUserPasswordErr     = Err{20203, "user origin password is invalid"}
+	UpdateUserPasswordErr     = Err{20204, "update user password is failed"}
+	NotFoundUser              = Err{20205, "not found user"}
+	UpdateMerchantStatusErr   = Err{20300, "update merchant status is failed"}
+	NotFoundMerchant          = Err{20301, "not found merchant"}
+	CreateMerchantRechargeErr = Err{20302, "create recharge apply is failed"}
+	NotFoundAssetApplyErr     = Err{20400, "not found recharge apply"}
 
 	//订单相关错误码
 	NoAccountIdOrDistributorIdErr = Err{20501, "accountId or distributorId is null"}
