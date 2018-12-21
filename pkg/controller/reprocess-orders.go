7323d931b77f97f7fe857f526271baac7cd70e96
xyyhcl <1245815983@qq.com>
2018-12-21 14:05:32 +0800

    add reprocess order api and service

--- pkg/controller/reprocess-orders.go
+++ pkg/controller/reprocess-orders.go
@@ -4,7 +4,9 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"yuudidi.com/pkg/protocol/response"
+	"strconv"
+	"yuudidi.com/pkg/service"
+	"yuudidi.com/pkg/utils"
 )
 
 // @Summary 失败订单再处理
@@ -12,24 +14,22 @@ import (
 // @Description 失败订单再处理api
 // @Accept  json
 // @Produce  json
-// @Param body body response.ReprocessOrderRequest true "请求体"
+// @Param  origin_order query string true "平台商订单id"
+// @Param  distributor_id query string true "平台商id"
 // @Success 200 {object} response.ReprocessOrderResponse "成功（status为success）失败（status为fail）都会返回200"
-// @Router /c/reprocess-order [post]
-func ReprocessOrder(c *gin.Context){
-	var req response.ReprocessOrderRequest
-	c.ShouldBind(&req)
-	var ret response.ReprocessOrderResponse
-	ret.Status=response.StatusSucc
-	ret.ErrCode=123
-	ret.ErrMsg="reprecess success"
-	ret.Data=[]response.ReprocessOrderEntity{
-		{
-			Url:          "www.otc.com",
-			OrderSuccess: "Notify Order Created",
-			TotalCount:   "12",
-			OrderNo:      "12332",
-			OrderType:    "2",
-		},
+// @Router /c/reprocess-order [get]
+func ReprocessOrder(c *gin.Context) {
+
+	origin_order := c.Query("origin_order")
+	distributor_id := c.Query("distributor_id")
+	data, err := strconv.ParseInt(distributor_id, 10, 64)
+	if err != nil {
+		utils.Log.Error("distributor_id convet from string to int64 wrong")
 	}
-	c.JSON(200,ret)
+
+	url := service.ReprocessOrder(origin_order, data)
+
+	c.Redirect(200, url)
+
 }
+
---
+++ pkg/service/reprocess-order.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"yuudidi.com/pkg/protocol/response"
+	"yuudidi.com/pkg/utils"
+)
+
+func ReprocessOrder(origin_order string, distributorId int64) string {
+	var url string
+	orderRet := GetOrderByOriginOrderAndDistributorId(origin_order, distributorId)
+	if orderRet.Status==response.StatusFail||&orderRet.Data==nil{
+		utils.Log.Error("can not find order according origin_order and distributorId")
+		return ""
+	}
+	orderNumber := orderRet.Data[0].OrderNumber
+	url = url + orderNumber
+	return url
+
+}
