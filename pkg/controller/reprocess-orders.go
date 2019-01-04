15b8207332fe2a3251474f80da8b345d32714c57
xyyhcl <1245815983@qq.com>
2019-01-04 14:00:25 +0800

    add switch for api sign

--- pkg/controller/reprocess-orders.go
+++ pkg/controller/reprocess-orders.go
@@ -28,29 +28,32 @@ func ReprocessOrder(c *gin.Context) {
 	}
 
 	//签名认证
+	if utils.Config.Get("signswitch.sign") == "on" {
+		apiKey := c.Query("apiKey")
+		sign := c.Query("sign")
 
-	apiKey := c.Query("apiKey")
-	sign := c.Query("sign")
+		method := c.Request.Method
+		host := c.Request.Host
+		uri := c.Request.URL.Path
+		secretKey := service.GetSecretKeyByApiKey(apiKey)
+		if secretKey == "" {
+			utils.Log.Error("can not get secretkey according to apiKey=[%s] ", apiKey)
+			return
+		}
 
-	method := c.Request.Method
-	host := c.Request.Host
-	uri := c.Request.URL.Path
-	secretKey := service.GetSecretKeyByApiKey(apiKey)
-	if secretKey == "" {
-		utils.Log.Error("can not get secretkey according to apiKey=[%s] ", apiKey)
-		return
+		if err != nil {
+			utils.Log.Error("struct convert to string wrong,[%v]", err)
+		}
+		str := service.GenSignatureWith2(method, host, uri, origin_order, distributor_id, apiKey)
+		sign1, _ := service.HmacSha256Base64Signer(str, secretKey)
+		if sign != sign1 {
+			utils.Log.Error("sign is not right,sign=[%v]", sign1)
+			c.JSON(403, "you do not have the right to createOrder")
+			return
+		}
+
 	}
 
-	if err != nil {
-		utils.Log.Error("struct convert to string wrong,[%v]", err)
-	}
-	str := service.GenSignatureWith2(method, host, uri, origin_order, distributor_id, apiKey)
-	sign1, _ := service.HmacSha256Base64Signer(str, secretKey)
-	if sign != sign1 {
-		utils.Log.Error("sign is not right,sign=[%v]", sign1)
-		c.JSON(403, "you do not have the right to createOrder")
-		return
-	}
 
 
 	orderNumber := service.ReprocessOrder(origin_order, data)
