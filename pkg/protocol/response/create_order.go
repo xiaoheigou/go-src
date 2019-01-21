93367d7026f0d511c224466f479d82ef8d901659
xyyhcl <1245815983@qq.com>
2019-01-21 10:17:53 +0800

    modify the param name in  json

--- pkg/protocol/response/create_order.go
+++ pkg/protocol/response/create_order.go
@@ -4,22 +4,22 @@ import "yuudidi.com/pkg/models"
 
 type CreateOrderResult struct {
 	OrderNumber    string  `json:"orderNumber"`
-	RedirectUrl    string  `json:"redirect_url"`
+	RedirectUrl    string  `json:"redirectUrl"`
 	Direction      int     `json:"direction"`
-	OriginOrder    string  `json:"origin_order"`
+	OriginOrder    string  `json:"originOrder"`
 	AccountID      string  `json:"account"`
 	DistributorID  int64   `json:"distributor"`
-	CurrencyCrypto string  `json:"currency_crypto"`
-	CurrencyFiat   string  `json:"currency_fiat"`
+	CurrencyCrypto string  `json:"currencyCrypto"`
+	CurrencyFiat   string  `json:"currencyFiat"`
 	Quantity       float64 `json:"quantity"`
 	Price          float32 `json:"price"`
 	Amount         float64 `json:"amount"`
-	PayType        uint    `json:"pay_type"`
-	QrCode         string  `gorm:"type:varchar(255)" json:"qr_code"`
-	Name           string  `gorm:"type:varchar(100)" json:"name"`
-	BankAccount    string  `gorm:"" json:"bank_account"`
-	Bank           string  `gorm:"" json:"bank"`
-	BankBranch     string  `gorm:"" json:"bank_branch"`
+	PayType        uint    `json:"payType"`
+	QrCode         string  `json:"qrCode"`
+	Name           string  `json:"name"`
+	BankAccount    string  `bankAccount"`
+	Bank           string  `json:"bank"`
+	BankBranch     string  `json:"bankBranch"`
 }
 
 type CreateOrderRet struct {
