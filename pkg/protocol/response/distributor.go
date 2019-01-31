13dfbfac360b1367df9d0105e31087094e481bf0
lucky1024 <xxx@xxx.com>
2019-01-31 17:08:48 +0800

    Add empty checking for creating/updating distributor

--- pkg/protocol/response/distributor.go
+++ pkg/protocol/response/distributor.go
@@ -14,15 +14,15 @@ type CreateDistributorsRet struct {
 }
 
 type CreateDistributorsArgs struct {
-	Name                     string  `json:"name" binding:"required" example:"test"`
-	Phone                    string  `json:"phone" binding:"required" example:"13112345678"`
-	Domain                   string  `json:"domain" binding:"required" example:"baidu.com"`
-	PageUrl                  string  `json:"page_url" binding:"required" example:"1"`
-	ServerUrl                string  `json:"server_url" binding:"required" example:"1"`
-	ApiKey                   string  `json:"api_key" binding:"required" example:"13112345678"`
-	ApiSecret                string  `json:"api_secret" binding:"required" example:"13112345678"`
-	Username                 string  `json:"username"`
-	Password                 string  `json:"password"`
+	Name      string `json:"name" binding:"required" example:"test"`
+	Phone     string `json:"phone" example:"13112345678"`
+	Domain    string `json:"domain" binding:"required" example:"baidu.com"`
+	PageUrl   string `json:"page_url" example:"1"`
+	ServerUrl string `json:"server_url" example:"1"`
+	ApiKey    string `json:"api_key" binding:"required" example:"13112345678"`
+	ApiSecret string `json:"api_secret" binding:"required" example:"13112345678"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
 	//AppUserWithdrawalFeeRate float64 `json:"app_user_withdrawal_fee_rate"`
 }
 
@@ -32,12 +32,7 @@ type UpdateDistributorsRet struct {
 }
 
 type UpdateDistributorsArgs struct {
-	Name  string `json:"name" binding:"required" example:"test"`
-	Phone string `json:"phone" binding:"required" example:"13112345678"`
-	//平台商状态 0: 申请 1: 正常 2: 冻结
-	Status    int    `json:"status" binding:"required" example:"1"`
-	PageUrl   string `json:"page_url" binding:"required" example:"1"`
-	ServerUrl string `json:"server_url" binding:"required" example:"1"`
-	ApiKey    string `json:"api_key" binding:"required" example:"13112345678"`
-	ApiSecret string `json:"api_secret" binding:"required" example:"13112345678"`
+	Name   string `json:"name" binding:"required" example:"test"`
+	Phone  string `json:"phone" example:"13112345678"`
+	Domain string `json:"domain" binding:"required" example:"baidu.com"`
 }
