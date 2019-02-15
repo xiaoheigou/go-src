214f5bb225756f4361b402ad8e746fa2c1385bdf
lucky1024 <xxx@xxx.com>
2019-02-15 16:55:15 +0800

    Add error msg if type assertion fail

--- pkg/service/fulfillment_engine.go
+++ pkg/service/fulfillment_engine.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/pkg/errors"
 	"github.com/shopspring/decimal"
+	"reflect"
 	"strconv"
 	"time"
 	"yuudidi.com/pkg/models"
@@ -72,21 +73,33 @@ func getOrderToFulfillFromMapStrings(values map[string]interface{}) OrderToFulfi
 
 	if distributorN, ok := values["distributor"].(json.Number); ok {
 		distributorID, _ = distributorN.Int64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[distributor] is %T", reflect.TypeOf(values["distributor"]).Kind())
 	}
 	if directN, ok := values["direction"].(json.Number); ok {
 		direct, _ = directN.Int64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[direction] is %T", reflect.TypeOf(values["direction"]).Kind())
 	}
 	if payTN, ok := values["pay_type"].(json.Number); ok {
 		payT, _ = payTN.Int64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[pay_type] is %T", reflect.TypeOf(values["pay_type"]).Kind())
 	}
 	if quantityN, ok := values["quantity"].(json.Number); ok {
 		quantity, _ = quantityN.Float64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[quantity] is %T", reflect.TypeOf(values["quantity"]).Kind())
 	}
 	if priceN, ok := values["price"].(json.Number); ok {
 		price, _ = priceN.Float64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[price] is %T", reflect.TypeOf(values["price"]).Kind())
 	}
 	if amountN, ok := values["amount"].(json.Number); ok {
 		amount, _ = amountN.Float64()
+	} else {
+		utils.Log.Errorf("Type assertion fail, type of values[amount] is %T", reflect.TypeOf(values["amount"]).Kind())
 	}
 	var qrCode, name, bank, bankAccount, bankBranch string
 	if values["qr_code"] != nil {
