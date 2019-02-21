7b5573558e4d373acbcac575edd9d88709512c2b
lucky1024 <xxx@xxx.com>
2019-02-21 17:50:06 +0800

    Add composite index

--- pkg/models/bill.go
+++ pkg/models/bill.go
@@ -10,11 +10,11 @@ type ReceivedBill struct {
 	// 上传者币商id
 	UploaderUid int64 `gorm:"column:uploader_uid;index;not null" json:"uploader_uid"`
 	// 账单类型 1:微信,2:支付宝
-	PayType int `gorm:"column:pay_type;type:tinyint(2)" json:"pay_type"`
+	PayType int `gorm:"column:pay_type;type:tinyint(2);index:idx_received_pay_bill_id:not null" json:"pay_type"`
 	// 支付宝或微信用户支付id，前端App通过hook方式可以拿到
 	UserPayId string `gorm:"column:user_pay_id" json:"user_pay_id"`
 	// 支付宝或微信的账单Id
-	BillId string `gorm:"type:varchar(191);unique_index;not null" json:"bill_id"`
+	BillId string `gorm:"column:bill_id;type:varchar(191);index:idx_received_pay_bill_id;not null" json:"bill_id"`
 	// 从账单备注字段中得到的jrdidi订单号
 	OrderNumber string `gorm:"type:varchar(191);" json:"order_number"`
 	// 账单的人民币金额
