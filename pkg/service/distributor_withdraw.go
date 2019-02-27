3a76cb24d13aed6197c41dc589f978cdfa2e95ff
lucky1024 <xxx@xxx.com>
2019-02-27 20:56:35 +0800

    Add responseFormat to hac-sha256 payload

--- pkg/service/distributor_withdraw.go
+++ pkg/service/distributor_withdraw.go
@@ -37,6 +37,9 @@ func buildWithdrawParams(arg response.DistributorWithdrawArgs, distributorId int
 	params["appServerNotifyUrl"] = arg.AppServerNotifyUrl
 	params["appReturnPageUrl"] = arg.AppReturnPageUrl
 
+	// 让下单接口返回json格式的数据
+	params["responseFormat"] = "json"
+
 	urlParams := url.Values{}
 	for k := range params {
 		urlParams.Add(k, params[k])
@@ -83,7 +86,9 @@ func fireWithdraw(arg response.DistributorWithdrawArgs, distributorId string, us
 	appSignContent, _ := HmacSha256Base64Signer(paramsWithUrlEncoded, distributor.ApiSecret)
 
 	//apiUrl := "https://jrdidi.com/order/withdraw/create?appId=10001&apiKey=c6aec828fe514980&inputCharset=UTF-8&apiVersion=1.1&appSignType=HMAC-SHA256&appSignContent=" + content
-	reqUrl := "http://13.250.12.109:8084/order/withdraw/create"
+	utils.Config.GetString("jrdidiurl.api")
+
+	reqUrl := utils.Config.GetString("jrdidiurl.api") + "/order/withdraw/create"
 	apiUrl := fmt.Sprintf("%s?appId=%s&apiKey=%s&inputCharset=UTF-8&apiVersion=1.1&appSignType=HMAC-SHA256&appSignContent=%s",
 		reqUrl, distributorId, distributor.ApiKey, appSignContent)
 
